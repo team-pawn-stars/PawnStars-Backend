3c652dd0324d227346e177d185250f407318836c
Sangmin In <python@istruly.sexy>
2019-05-01 08:52:45 +0900

    FIX url

--- pawn_stars_api/urls.py
+++ pawn_stars_api/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('signin', obtain_jwt_token),
-    path('signup/buyer', views.BuyerUserView.as_view()),
-    path('signup/seller', views.SellerUserView.as_view()),
+    path('signup/buyer/', views.BuyerUserView.as_view()),
+    path('signup/seller/', views.SellerUserView.as_view()),
     path('pawn/', views.PawnListView.as_view()),
 ]
