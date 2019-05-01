7f478e12c04f6390014b68f8acf511cb1b1d293f
Sangmin In <python@istruly.sexy>
2019-05-01 22:29:10 +0900

    fix signin url

--- pawn_stars_api/urls.py
+++ pawn_stars_api/urls.py
@@ -5,7 +5,7 @@ from rest_framework_jwt.views import obtain_jwt_token
 from . import views
 
 urlpatterns = [
-    path('signin', obtain_jwt_token),
+    path('signin/', obtain_jwt_token),
     path('signup/buyer/', views.BuyerUserView.as_view()),
     path('signup/seller/', views.SellerUserView.as_view()),
     path('pawn/', views.PawnListView.as_view()),
